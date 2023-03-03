24e4cd55341fa7230fd13c01ba85a3fab254428b
PavelBui <89442856+PavelBui@users.noreply.github.com>
2023-03-03 13:45:46 +0300

    Updated userRepository

--- src/main/java/com/epam/learning/backendservices/springfoundation/data/UserRepository.java
+++ src/main/java/com/epam/learning/backendservices/springfoundation/data/UserRepository.java
@@ -4,5 +4,11 @@ import com.epam.learning.backendservices.springfoundation.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {}
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findById(UUID id);
+
+    void deleteById(UUID id);
+}
