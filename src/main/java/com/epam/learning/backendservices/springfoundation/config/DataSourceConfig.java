9f499cea6761b0e575be22d027e690e66a629a36
PavelBui <89442856+PavelBui@users.noreply.github.com>
2023-03-06 18:31:19 +0300

    Delete DataSourceConfig.java

--- src/main/java/com/epam/learning/backendservices/springfoundation/config/DataSourceConfig.java
+++
@@ -1,7 +0,0 @@
-package com.epam.learning.backendservices.springfoundation.config;
-
-public interface DataSourceConfig {
-    String getUrl();
-    String getUsername();
-    String getPassword();
-}
